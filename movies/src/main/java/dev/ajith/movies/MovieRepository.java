f981d7d93b729a803fb3f259699db10f55697a2c
Ajith <ajith.penmatsa001@gmail.com>
2025-01-27 22:42:13 +1300

    made changes so that the used can find a movie using the idmbId

--- movies/src/main/java/dev/ajith/movies/MovieRepository.java
+++ movies/src/main/java/dev/ajith/movies/MovieRepository.java
@@ -1,8 +1,11 @@
 package dev.ajith.movies;
 
+import java.util.Optional;
+
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface MovieRepository extends MongoRepository<Movie, ObjectId> {
+    Optional<Movie> findMovieByImdbId(String imdbId);
     
 }
