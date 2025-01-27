518589001741bf0f5cd3e723660b0690ed67b99f
Shadowwing1960 <98994815+Shadowwing1960@users.noreply.github.com>
2025-01-28 00:35:53 +1300

    Set up axiosConfig.js and made it so that it can load out data api onto App.js

--- movies/src/main/java/dev/ajith/movies/MovieController.java
+++ movies/src/main/java/dev/ajith/movies/MovieController.java
@@ -2,11 +2,13 @@ package dev.ajith.movies;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/api/v1/movies")
 public class MovieController {
 
